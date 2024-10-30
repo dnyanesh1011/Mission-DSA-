eb2f7a25fc89313ab8fb012aec4b34584bb73846
Dnyanesh Fulsundar <170419176+dnyanesh1011@users.noreply.github.com>
2024-10-30 18:41:04 +0530

    recursion day2
    just printing the n-1 integer form n upto 1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num1 = 5;
+    int num2 = 10;
+
+    int num3 = num1 + num2;
+
+    cout << "The Addition is: " << num3 << endl;
+
+    return 0;
+}
